f142a914ebe13891857cecdd680a026ae44b24e4
samirvithlani <samir.vithlani83955@gmail.com>
2023-09-22 19:45:54 +0530

    basic

---
+++ basics/basic1.py
@@ -0,0 +1,27 @@
+print("hello this is python !!!",end=",")
+print("hello all !!!")
+
+no =10 
+print(no)
+print("value of no = ",no)
+print("type of no = ",type(no))
+
+per = 89.90
+print("user per =",per," !!")
+print("type of per = ",type(per))
+
+c ='c'
+print("c = ",c)
+print("type of c = ",type(c))
+
+name = "python"
+print("name = ",name)
+print("type of name = ",type(name))
+
+isActive = False
+print("isActive = ",isActive)
+print("type of isActive = ",type(isActive))
+
+x = None
+print("x = ",x)
+print("type of x = ",type(x))
