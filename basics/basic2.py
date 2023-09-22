176cabf6d3cf77bd2bbb874ce44fd21c76244741
samirvithlani <samir.vithlani83955@gmail.com>
2023-09-22 20:36:54 +0530

    input

---
+++ basics/basic2.py
@@ -0,0 +1,21 @@
+
+
+#no = input("enter no")
+'''
+no = int(input("enter no")) #explicit type casting
+print(no)
+print("type of no = ",type(no))
+#input function always return string "12" 
+
+per = float(input("enter per"))
+print("per = ",per)
+print("type of per = ",type(per))
+
+name = input("enter name")
+print("name = ",name)
+print("type of name = ",type(name))
+
+'''
+isMarried = bool(input("isMarried"))
+print("isMarried = ",isMarried)
+print("type of isMarried = ",type(isMarried))
---
+++ operators/arithmaric1.py
@@ -0,0 +1,10 @@
+x = int(input("Enter the value of x: "))   
+y = int(input("Enter the value of y: "))
+
+#c = x * y
+# c = x **y #power of x to y
+# print("Multiplication of x and y is: ", c)
+
+#d = x / y
+d = x // y
+print("Division of x and y is: ", d)
