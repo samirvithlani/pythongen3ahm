c5cf9e9b8d61c5c49e105d56b9f6ad8f33153c0e
samirvithlani <samir.vithlani83955@gmail.com>
2023-12-01 20:20:21 +0530

    dictonary

--- ds/dictonary/dict7.py
+++ ds/dictonary/dict7.py
@@ -3,5 +3,17 @@ print(teams)
 
 print("csk ..",teams["csk"])
 
-for k,v in teams["csk"].items():
-    print(k,"--",v)
+# for k,v in teams["csk"].items():
+#     print(k,"--",v)
+    
+
+
+for k,v in teams.items():
+    print(k)
+    for k1,v1 in v.items():
+        print(k1,"--",v1)
+    
+    print("-----")
+    
+    
+    
